b662f3fe172acbb11f453195e6449177efca23f1
Alain Ghawi <ghawialain@gmail.com>
2023-07-10 20:22:43 +0300

    Add the customer controller and error logger tests

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        //Name your test using the following convention:
+        //[MethodName]_[Scenario]_[ExpectedBehavior]
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            //Arrange
+            var controller = new CustomerController();
+
+            //Act
+            var result = controller.GetCustomer(0);
+
+            //Assert
+            //NotFound exact type
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFound or one of its derivatives (e.g. DocumentNotFound inheriting NotFound)
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            //Arrange
+            var controller = new CustomerController();
+
+            //Act
+            var result = controller.GetCustomer(1);
+
+            //Assert
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
---
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        //Name your test using the following convention:
+        //[MethodName]_[Scenario]_[ExpectedBehavior]
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("Test error");
+
+            Assert.That(logger.LastError, Is.EqualTo("Test error"));
+        }
+    }
+}
