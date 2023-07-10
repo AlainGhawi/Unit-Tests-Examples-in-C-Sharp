d69908af33b3ea9ce1d4bb351478aa2d768d3119
Alain Ghawi <ghawialain@gmail.com>
2023-07-10 20:08:02 +0300

    Changes to the tests
    Changes to the tests

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseStringWithStrongElement()
+        {
+            //Arrange
+            var formatter = new HtmlFormatter();
+
+            //Act
+            var result = formatter.FormatAsBold("alain");
+
+            //Assert
+            //Specific
+            Assert.That(result, Is.EqualTo("<strong>alain</strong>"));
+
+            //More general
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("alain"));
+        }
+    }
+}
--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -42,6 +43,18 @@ namespace TestNinja.UnitTests
             var result = _math.Max(a, b);
             //Assert
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+            //Instead of 3 assertions, this how you can assert an array
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
         }
 
     }
