e8fad279f6dd9d666d8b262219a39e107ec96ea1
Alain Ghawi <ghawialain@gmail.com>
2023-07-11 19:30:25 +0300

    Add the demerit points calculator tests

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        //Name your test using the following convention:
+        //[MethodName]_[Scenario]_[ExpectedBehavior]
+
+        private DemeritPointsCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(301)]
+        [TestCase(-1)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ReturnsArgumentOutOfRangeException(int speed)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.CalculateDemeritPoints(speed));
+        }
+
+        [Test]
+        [TestCase(60, 0)]
+        [TestCase(65, 0)]
+        [TestCase(70, 1)]
+        [TestCase(300, 47)]
+        [TestCase(69, 0)]
+        public void CalculateDemeritPoints_SpeedIsWithinRange_ReturnsDemeritPoints(int speed, int expectedResult)
+        {
+            Assert.That(() => _calculator.CalculateDemeritPoints(speed), Is.EqualTo(expectedResult));
+        }
+    }
+}
